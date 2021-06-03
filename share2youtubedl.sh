8680a85ab91b6f1ad6811bfdf3b939ee45eb7cf0
PointVoider <54562368+PointVoider@users.noreply.github.com>
2021-06-03 12:46:37 +0000

    Create share2youtubedl.sh

--- share2youtubedl.sh
+++ share2youtubedl.sh
@@ -3,20 +3,20 @@
 # This is a termux-url-opener script to do diffrent tasks on my Android phone 
 
 url=$1
-echo "YouTube-dl :\nWhat should I do with $url ?"
-echo "y) download video to ytdl-folder"
-echo "u) download video and convert it to mp3 (music-folder)"
-echo "x) nothing"
+echo "YouTube-dl :\nApa yg harus dilakukan pada link $url ?"
+echo "y) unduh video dengan kualitas tertinggi"
+echo "u) unduh video kemudian ubah menjadi mp3 (musik)"
+echo "x) tidak ada"
 
 read -s -k CHOICE
 case $CHOICE in
     y)
-        echo "Downloading Video With Highest Quality Available\n"
+        echo "Mulai mengunduh video dengan kualitas terbaik yang tersedia\n"
         youtube-dl $url
         read -s -k '?Press any key to continue.'
  ;;
     u)
-    echo "Downloading Audio With Highest Quality Available\n"
+    echo "Mulai mengunduh musik dengan kualitas terbaik yang tersedia\n\n"
  youtube-dl --ignore-config -f bestaudio --extract-audio --audio-format mp3  --add-metadata --embed-thumbnail --output "/data/data/com.termux/files/home/storage/shared/Youtube-DL/Music/%(title)s.%(ext)s" $url
  read -s -k '?Press any key to continue.'
  ;;
